27a952fad77ceff039362e3503448c09a771887a
gzkp <chenruichen@kp99.cn>
2019-02-16 17:55:17 +0800

    移植 ZYFoundation

---
+++ KPIOSFoundation/ZYFoundation.h
@@ -0,0 +1,42 @@
+//
+//  ZYYObjcLib.h
+//  ZYYObjcLib
+//
+//  Created by zyyuann on 15/12/25.
+//  Copyright © 2015年 ZYY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZYYObjcLib.
+FOUNDATION_EXPORT double ZYYObjcLibVersionNumber;
+
+//! Project version string for ZYYObjcLib.
+FOUNDATION_EXPORT const unsigned char ZYYObjcLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZYYObjcLib/PublicHeader.h>
+
+#import <ZYFoundation/GeneralUse.h>
+#import <ZYFoundation/ZYPublicDefine.h>
+#import <ZYFoundation/DeviceBaseData.h>
+
+#import <ZYFoundation/DBToParser.h>
+#import <ZYFoundation/JsonToParser.h>
+#import <ZYFoundation/BridgeClientSDK.h>
+#import <ZYFoundation/NetWorkManage.h>
+#import <ZYFoundation/CheckNetwork.h>
+
+#import <ZYFoundation/AnimTimeManege.h>
+#import <ZYFoundation/TimingTask.h>
+
+#import <ZYFoundation/FileUse.h>
+#import <ZYFoundation/FileUseOther.h>
+#import <ZYFoundation/ShareTable.h>
+
+#import <ZYFoundation/SecurityPolicy.h>
+#import <ZYFoundation/MultiLanguage.h>
+#import <ZYFoundation/TCPLinkManage.h>
+
+
+#import <ZYFoundation/GeneralUIUse.h>
+#import <ZYFoundation/LCMD5Tool.h>
